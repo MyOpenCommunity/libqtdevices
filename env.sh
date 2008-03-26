be5672b79b2a28ed63f5868f8ba9e1fc5e49b3a2
Stefano Fedrigo <aleph@develer.com>
2008-03-26 15:53:16 +0100

    BTouch: fix per compilazione

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/bin/echo .
 
 export QTDIR=$HOME/src/bticino/myhome/qt-embedded-free-3.3.4
-export QMAKESPEC=qws/linux-H4684_IP-g++
+export QMAKESPEC=$HOME/src/bticino/myhome/BTouch
 export PATH=$QTDIR/bin:/opt/bt-toolch-soft_32/bin:$PATH
