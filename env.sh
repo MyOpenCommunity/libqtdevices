49c426ae672e8b583e9249c64762d738dfbe71c2
Gianni Valdambrini <aleister@develer.com>
2008-06-06 19:08:01 +0200

    Aggiunto al repository btintmm e btclientlib, alcuni aggiustamenti agli script di build

--- env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/echo .
-
-export QTDIR=$HOME/src/bticino/myhome/qt-embedded-free-3.3.4
-export QMAKESPEC=$HOME/src/bticino/myhome/BTouch
-export PATH=$QTDIR/bin:/opt/bt-toolch-soft_32/bin:$PATH
--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sudo rm -f $HOME/src/nfs_btouch/etc/pointercal
-sudo cp BTouch /nfs_btouch/home/bticino/bin
+cp BTouch /nfs_btouch/home/bticino/bin
