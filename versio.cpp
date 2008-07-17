d3d760610806381550fe8c4fb52d1461349d48c5
Stefano Fedrigo <aleph@develer.com>
2008-07-17 17:53:47 +0200

    Altro test per git-commit...

--- versio.cpp
+++ versio.cpp
@@ -45,7 +45,7 @@ versio::versio( QWidget *parent,const char *name , unsigned int f)
      datiGen->setLineWidth(3);
      //datiGen->setText(" H/L/N/NT4684");
      datiGen->setText( model );
-//     datiGen->setText("art. H4684\n\nFIRMWARE\nPIC_REL\nHARDWARE");       
+//     datiGen->setText("art. H4684\n\nFIRMWARE\nPIC_REL\nHARDWARE");
      
 #if defined (BTWEB) ||  defined (BT_EMBEDDED)
      setCursor (QCursor (blankCursor));
