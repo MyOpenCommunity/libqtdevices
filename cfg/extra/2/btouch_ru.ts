3477794757f68c03322c906fd90282aed4952358
Gianni Valdambrini <aleister@develer.com>
2008-09-10 14:54:18 +0200

    BTouch: aggiunti file di traduzione per le varie lingue e modificato quindi file di progetto.

---
+++ cfg/extra/2/btouch_ru.ts
@@ -0,0 +1,195 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>Calibrate</name>
+    <message>
+        <source>Click here!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FSBannManual</name>
+    <message>
+        <source>Manual</source>
+        <translation>Ручной</translation>
+    </message>
+</context>
+<context>
+    <name>MediaPlayWindow</name>
+    <message>
+        <source>of</source>
+        <translation>из</translation>
+    </message>
+    <message>
+        <source>Track:</source>
+        <translation>Дорожка:</translation>
+    </message>
+    <message>
+        <source>Artist:</source>
+        <translation>Артист:</translation>
+    </message>
+    <message>
+        <source>Album:</source>
+        <translation>Альбом:</translation>
+    </message>
+    <message>
+        <source>Length:</source>
+        <translation>Время:</translation>
+    </message>
+</context>
+<context>
+    <name>PowerAmplifier</name>
+    <message>
+        <source>Preset</source>
+        <translation>Настройка</translation>
+    </message>
+    <message>
+        <source>Treble</source>
+        <translation>Высокие</translation>
+    </message>
+    <message>
+        <source>Bass</source>
+        <translation>Низкие</translation>
+    </message>
+    <message>
+        <source>Balance</source>
+        <translation>Баланс</translation>
+    </message>
+    <message>
+        <source>Loud</source>
+        <translation>Громко</translation>
+    </message>
+</context>
+<context>
+    <name>PowerAmplifierPreset</name>
+    <message>
+        <source>Normal</source>
+        <translation>Нормальный</translation>
+    </message>
+    <message>
+        <source>Dance</source>
+        <translation>Танцевальный</translation>
+    </message>
+    <message>
+        <source>Pop</source>
+        <translation>Поп</translation>
+    </message>
+    <message>
+        <source>Rock</source>
+        <translation>Рок</translation>
+    </message>
+    <message>
+        <source>Classical</source>
+        <translation>Классический</translation>
+    </message>
+    <message>
+        <source>Jazz</source>
+        <translation>Джаз</translation>
+    </message>
+    <message>
+        <source>Party</source>
+        <translation>Вечеринка</translation>
+    </message>
+    <message>
+        <source>Soft</source>
+        <translation>Спокойный</translation>
+    </message>
+    <message>
+        <source>Full Bass</source>
+        <translation>Низкие</translation>
+    </message>
+    <message>
+        <source>Full Treble</source>
+        <translation>Высокие</translation>
+    </message>
+    <message>
+        <source>Preset</source>
+        <translation>Настройка</translation>
+    </message>
+</context>
+<context>
+    <name>SourceChoice</name>
+    <message>
+        <source>IP Radio</source>
+        <translation>IP Радио</translation>
+    </message>
+    <message>
+        <source>Servers</source>
+        <translation>Серверы</translation>
+    </message>
+</context>
+<context>
+    <name>StopngoPage</name>
+    <message>
+        <source>Self-recluser</source>
+        <translation>АвтоПерезапуск</translation>
+    </message>
+    <message>
+        <source>Test</source>
+        <translation>Тест</translation>
+    </message>
+    <message>
+        <source>Self-test</source>
+        <translation>Самоанализ</translation>
+    </message>
+</context>
+<context>
+    <name>antintrusione</name>
+    <message>
+        <source>technical</source>
+        <translation>технич.тревога</translation>
+    </message>
+    <message>
+        <source>intrusion</source>
+        <translation>вторжение</translation>
+    </message>
+    <message>
+        <source>tamper</source>
+        <translation>самозащита</translation>
+    </message>
+    <message>
+        <source>anti-panic</source>
+        <translation>срочный вызов</translation>
+    </message>
+</context>
+<context>
+    <name>call_notifier</name>
+    <message>
+        <source>Unknown</source>
+        <translation>Неизвестный</translation>
+    </message>
+    <message>
+        <source>Staircase light</source>
+        <translation>Свет лестница</translation>
+    </message>
+    <message>
+        <source>Door lock</source>
+        <translation>Замок</translation>
+    </message>
+</context>
+<context>
+    <name>sveglia</name>
+    <message>
+        <source>once</source>
+        <translation>однажды</translation>
+    </message>
+    <message>
+        <source>always</source>
+        <translation>всегда</translation>
+    </message>
+    <message>
+        <source>mon-fri</source>
+        <translation>пон-пят</translation>
+    </message>
+    <message>
+        <source>sat-sun</source>
+        <translation>суб-воскр</translation>
+    </message>
+</context>
+<context>
+    <name>tastiera</name>
+    <message>
+        <source>PASSWORD:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
