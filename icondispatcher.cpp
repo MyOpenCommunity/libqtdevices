e3186055e5dc68e1c9abd8f7132d2b5b7cb5d020
Gianni Valdambrini <aleister@develer.com>
2008-09-17 15:22:10 +0200

    BTouch: riscritta cache delle icone per usare un QHash al posto di un QCache (che in qt4 non puo' essere usato perche' mantiene l'ownership degli oggetti)

--- icondispatcher.cpp
+++ icondispatcher.cpp
@@ -1,74 +1,48 @@
 /**
  * \file
  * <!--
- * Copyright 2007 Develer S.r.l. (http://www.develer.com/)
+ * Copyright 2007, 2008 Develer S.r.l. (http://www.develer.com/)
  * All rights reserved.
  * -->
  *
  * \brief Icon Cache Class
  *
- * This class uses an internal QCache to implement a cache for the icons used in banner objects.
+ * This class implement a cache for the icons used in banner objects.
  * The class creates, dispatches and destroys the needed QPixmap.
  *
  * \author Alessandro Della Villa <kemosh@develer.com>
+ * \author Gianni Valdambrini <aleister@develer.com>
  */
 
-#include "icondispatcher.h"
 
-#include "qfile.h"
+#include "icondispatcher.h"
 #include "main.h"
 
-IconDispatcher::IconDispatcher(QObject *parent, const char *name)
- : QObject(parent, name)
-{
-	/*
-	 * Create Internal QCache object, uses three parameters:
-	 *
-	 * \param size is the max number of elements in Cache, best if it is a prime number
-	 *
-	 * \param maxCost is the max total cost of the CACHE (before to erase elements)
-	 * the cost is given at the insert time but we gave cost=0 for each element
-	 *
-	 * \param caseSensitive tells if the hash keys of CACHE are case sensitive or not
-	 */
-	const int maxCost = 100;
-	const int size = 199;
-	const bool caseSensitive = TRUE;
-	cache = new QCache<QPixmap>(maxCost, size, caseSensitive);
-}
+#include <QFile>
+#include <QDebug>
 
+IconDispatcher::IconDispatcher(QObject *parent) : QObject(parent) { }
 
 IconDispatcher::~IconDispatcher()
 {
-
-	// Create Iterator on Cache
-	QCacheIterator<QPixmap> iter = QCacheIterator<QPixmap>(*cache);
-
-	/*
-	 * Seek & Destroy cached elements,
-	 * the Method iter() of QCacheIterator returns an element and points the next one
-	 */
-	while (!iter.isEmpty())
+	QMutableHashIterator<QString, QPixmap*> it(cache);
+	while (it.hasNext())
 	{
-		delete iter();
+		it.next();
+		delete it.value();
 	}
-	
-	// Destroy Cache Object
-	delete cache;
 }
 
-
-QPixmap* IconDispatcher::getIconPointer(const char *name)
+QPixmap* IconDispatcher::getIconPointer(QString name)
 {
-	QPixmap *icon = cache->find(name, FALSE);
-
-	if (icon)
+	QHash<QString, QPixmap*>::const_iterator it = cache.find(name);
+	if (it != cache.end())
 	{
 		/*
 		 * If already exist in cache a pointer to QPixmap with hash=name
 		 * a pointer to this image is returned
-		 */
-		return icon;
+		 */	
+		return it.value();
 	}
 	else
 	{
@@ -76,42 +50,25 @@ QPixmap* IconDispatcher::getIconPointer(const char *name)
 		QPixmap* image_pointer;
 		image_pointer = new QPixmap();
 
-		cache->insert(name, image_pointer, 0, 0);
+		cache.insert(name, image_pointer);
 		return image_pointer;
 	}
 }
 
-
-QPixmap* IconDispatcher::getIcon(const char *name, const char * format, QPixmap::ColorMode mode)
+QPixmap* IconDispatcher::getIcon(QString name, const char * format, Qt::ImageConversionFlags flags)
 {
-	/// return NULL if file does not exist or if name == NULL
-	if (name == NULL)
-		return NULL;
+	if (name.isNull() || !QFile::exists(name))
+		return 0;
 
 	QPixmap* image_pointer = getIconPointer(name);
 	// a QPixmap is null when has zero width, zero height and no contents.
 	if (image_pointer->isNull())
+		image_pointer->load(name, format, flags);
+	else
 	{
-		if (!image_pointer->load(name, format, mode))
-		{
-			qDebug("Error loading icon '%s', using empty icon", name);
-			if (!image_pointer->load(ICON_VUOTO, format, mode))
-				qDebug("*** FATAL ***: empty icon not found!");
-		}
+		qDebug() << "Error loading icon '" << name << "', using empty icon";
+		if (!image_pointer->load(ICON_VUOTO))
+			qDebug("*** FATAL ***: empty icon not found!");
 	}
 	return image_pointer;
 }
-
-
-QPixmap* IconDispatcher::getIcon(const char *name, const char * format, int conversion_flags)
-{
-	/// return NULL if file does not exist or if name == NULL
-	if (  name == NULL || !QFile::exists(name) ) return NULL;
-
-	QPixmap* image_pointer = getIconPointer(name);
-	// a QPixmap is null when has zero width, zero height and no contents.
-	if (image_pointer->isNull())
-		image_pointer->load(name, format, conversion_flags);
-	return image_pointer;
-}
-
