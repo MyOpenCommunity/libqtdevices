207965a112a362a8c5fe6d71835d7b06008554c4
Gianni Valdambrini <aleister@develer.com>
2008-09-20 15:54:49 +0200

    BTouch: rindentato codice allarme e eliminati include non necessari

--- allarme.cpp
+++ allarme.cpp
@@ -1,13 +1,3 @@
-
-#include <qpixmap.h>
-#include <qwidget.h>
-#include <qcursor.h>
-#include <qdatetime.h>
-
-#include "openclient.h"
-#include "device.h"
-#include "frame_interpreter.h"
-#include "device_cache.h"
 #include "banner.h"
 #include "allarme.h"
 #include "bannfrecce.h"
@@ -15,22 +5,25 @@
 #include "main.h"
 #include "fontmanager.h"
 
+#include <qpixmap.h>
+#include <qwidget.h>
+#include <qcursor.h>
+
+
 /*****************************************************************
 ** Generic alarm
 ****************************************************************/	
 
-allarme::allarme(QWidget *parent, const QString & name, char *indirizzo, char *IconaDx,
-		 altype t): 
-    QFrame(parent)
+allarme::allarme(QWidget *parent, const QString & name, char *indirizzo, char *IconaDx, altype t): QFrame(parent)
 {
     qDebug("allarme::allarme()");
     qDebug("indirizzo = %s, IconaDx = %s, tipo = %d", indirizzo, IconaDx, t);
     type = t;
     SetIcons(IconaDx);
 #if defined (BTWEB) ||  defined (BT_EMBEDDED)
-    setCursor (QCursor (blankCursor));
-#endif  
-    setGeometry(0, 0, MAX_WIDTH, MAX_HEIGHT); 
+    setCursor(QCursor(blankCursor));
+#endif
+    setGeometry(0, 0, MAX_WIDTH, MAX_HEIGHT);
     setFixedSize(QSize(MAX_WIDTH, MAX_HEIGHT));
     descr->setText(name);
     connect(bnav, SIGNAL(backClick()), this, SIGNAL(Back()));
@@ -38,7 +31,7 @@ allarme::allarme(QWidget *parent, const QString & name, char *indirizzo, char *I
     connect(bnav, SIGNAL(downClick()), this, SIGNAL(Next()));
     connect(bnav, SIGNAL(forwardClick()), this, SIGNAL(Delete()));
     // ??
-    show() ;
+    show();
 }
 
 void allarme::SetIcons(char *icon)
@@ -46,30 +39,30 @@ void allarme::SetIcons(char *icon)
     qDebug("allarme::SetIcons()");
     QPixmap *Icon1 = new QPixmap();
     char *icon1;
-    switch(type) {
+    switch(type)
+	{
     case allarme::TECNICO:
-	icon1 = IMG_PATH "imgalltec.png";
-	break;
+		icon1 = IMG_PATH "imgalltec.png";
+		break;
     default:
-	icon1 = IMG_PATH "imgallintr.png";
-	break;
+		icon1 = IMG_PATH "imgallintr.png";
+		break;
     }
     Icon1->load(icon1);
     Immagine = new BtLabel(this, "icona_alarm");
     Immagine->setPixmap(*Icon1);
-    Immagine->setGeometry(MAX_WIDTH/2 - ICON_DIM/2, MAX_HEIGHT/(4*NUM_RIGHE), 
+    Immagine->setGeometry(MAX_WIDTH/2 - ICON_DIM/2, MAX_HEIGHT/(4*NUM_RIGHE),
 			  ICON_DIM, MAX_HEIGHT/NUM_RIGHE);
     delete(Icon1);
     descr = new BtLabel(this, "descr_alarm"); 
     QFont aFont;
-    FontManager::instance()->getFont( font_allarme_descr, aFont );
-    descr->setFont( aFont );
+    FontManager::instance()->getFont(font_allarme_descr, aFont);
+    descr->setFont(aFont);
     descr->setAlignment(AlignHCenter|AlignVCenter);
-    descr->setGeometry(0, MAX_HEIGHT/2 - (MAX_HEIGHT/NUM_RIGHE)/2, 
+    descr->setGeometry(0, MAX_HEIGHT/2 - (MAX_HEIGHT/NUM_RIGHE)/2,
 		       MAX_WIDTH, MAX_HEIGHT/NUM_RIGHE);
 
     bnav = new bannFrecce(this, "banner_nav", 4, icon);
-    //bnav->setGeometry(0, MAX_HEIGHT-NAV_DIM-NAV_DIM/8, MAX_WIDTH, NAV_DIM);
     bnav->setGeometry(0 , MAX_HEIGHT - MAX_HEIGHT/NUM_RIGHE,
 		      MAX_WIDTH, MAX_HEIGHT/NUM_RIGHE);
 }
@@ -79,7 +72,7 @@ void allarme::setBGColor(QColor c)
 {
     qDebug("allarme::setBGColor()");
     bnav->setBGColor(c);
-    setPaletteBackgroundColor(c);    
+    setPaletteBackgroundColor(c);
     Immagine->setPaletteBackgroundColor(c);
     descr->setPaletteBackgroundColor(c);
 }
@@ -88,7 +81,7 @@ void allarme::setFGColor(QColor c)
 {
     qDebug("allarme::setFGColor()");
     bnav->setFGColor(c);
-    setPaletteForegroundColor(c);  
+    setPaletteForegroundColor(c);
     Immagine->setPaletteForegroundColor(c);
     descr->setPaletteForegroundColor(c);
 }
@@ -106,9 +99,6 @@ void allarme::setFGColor(int r, int g, int b)
 void allarme::draw()
 {
     qDebug("allarme::draw()");
-    //Immagine->draw();
-    //descr->draw();
-    //bnav->draw();
 }
 
 void allarme::show()
