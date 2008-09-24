f6bf9e06c21da0c89e106f80f01b97efe74df4f9
Gianni Valdambrini <aleister@develer.com>
2008-09-24 11:49:38 +0200

    BTouch: rindentato bannonicons

--- bannOnIcons.cpp
+++ bannOnIcons.cpp
@@ -10,23 +10,21 @@
 
 #include "bannonicons.h"
 #include "main.h"
+
 #include <qlabel.h>
 #include <qpixmap.h>
 
 
-bannOnIcons::bannOnIcons( QWidget *parent,const char *name )
-        : banner( parent, name )
+bannOnIcons::bannOnIcons(QWidget *parent,const char *name) : banner(parent, name)
 {
-    addItem( BUT1 , 0 , 0 , BANNONICONS_BUT_DIM, BANNONICONS_BUT_DIM );
-    addItem( TEXT , 0 , BANNONICONS_BUT_DIM , MAX_WIDTH , MAX_HEIGHT/NUM_RIGHE-BANNONICONS_BUT_DIM );
-    addItem( ICON , BANNONICONS_BUT_DIM , 0 , BANNONICONS_BUT_DIM , BANNONICONS_BUT_DIM);
-    addItem( ICON2 , 2*BANNONICONS_BUT_DIM , 0 , BANNONICONS_BUT_DIM , BANNONICONS_BUT_DIM);
-    
-    connect(this,SIGNAL(sxClick()),this,SIGNAL(click()));
-    connect(this,SIGNAL(sxPressed()),this,SIGNAL(pressed()));
-    connect(this,SIGNAL(sxReleased()),this,SIGNAL(released()));
-   /*connect(this,SIGNAL(sxClick()),this,SIGNAL(sxClick()));
-    connect(this,SIGNAL(dxClick()),this,SIGNAL(dxClick()));*/
+	addItem(BUT1 , 0 , 0 , BANNONICONS_BUT_DIM, BANNONICONS_BUT_DIM);
+	addItem(TEXT , 0 , BANNONICONS_BUT_DIM , MAX_WIDTH , MAX_HEIGHT/NUM_RIGHE-BANNONICONS_BUT_DIM);
+	addItem(ICON , BANNONICONS_BUT_DIM , 0 , BANNONICONS_BUT_DIM , BANNONICONS_BUT_DIM);
+	addItem(ICON2 , 2*BANNONICONS_BUT_DIM , 0 , BANNONICONS_BUT_DIM , BANNONICONS_BUT_DIM);
+
+	connect(this,SIGNAL(sxClick()),this,SIGNAL(click()));
+	connect(this,SIGNAL(sxPressed()),this,SIGNAL(pressed()));
+	connect(this,SIGNAL(sxReleased()),this,SIGNAL(released()));
 }
 
 
