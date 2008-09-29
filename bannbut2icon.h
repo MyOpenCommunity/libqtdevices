b353ef4c5e1cb055f661f95f7c521c757e808d92
Gianni Valdambrini <aleister@develer.com>
2008-09-29 13:03:13 +0200

    BTouch: rindentato codice banbut2icon

--- bannbut2icon.h
+++ bannbut2icon.h
@@ -14,27 +14,6 @@
 #include "banner.h"
 #include <qwidget.h>
 
-/*!
-  \class bannBut2Icon
-  \brief This is a class that describes a banner with a button on the right, 
-  two icons in the middle and some text below
-  \author Ciminaghi
-  \date jun 2006
-*/   
-
-class bannBut2Icon : public banner
-{
-    Q_OBJECT
-public:
-     bannBut2Icon( QWidget *, const char * );
-
-    //void setIcons(const char *sxIcon=0, const char *dxIcon=0, 
-    //const char *onIcon=0);
-public slots:	
-private:
-  signals:
-
-};
 
 
 #define BANNBUT2ICON_TEXT_DIM_X          240
@@ -43,4 +22,20 @@ private:
 #define BANNBUT2ICON_ICON_DIM_Y           60
 #define BANNBUT2ICON_BUT_DIM              60
 
-#endif //BANNBUTICON_H
+
+/*!
+  \class bannBut2Icon
+  \brief This is a class that describes a banner with a button on the right, 
+  two icons in the middle and some text below
+  \author Ciminaghi
+  \date jun 2006
+*/
+
+class bannBut2Icon : public banner
+{
+Q_OBJECT
+public:
+	bannBut2Icon(QWidget *, const char *);
+};
+
+#endif //BANNBUT2ICON_H
