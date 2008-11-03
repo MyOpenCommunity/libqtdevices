ba44b8a11de5d0cf5b31d4b7fa1cba693e043fd2
Luca Ottaviano <lottaviano@develer.com>
2008-11-03 17:39:41 +0100

    BTouch: commit iniziale impostazioni LAN

---
+++ lansettings.cpp
@@ -0,0 +1,18 @@
+#include "lansettings.h"
+#include "bannondx.h"
+
+LanSettings::LanSettings(QWidget *parent) :
+	sottoMenu(parent)
+{
+	// create banners
+	bannOnDx *b = new bannOnDx(this, 0);
+	b->SetTextU("Prova");
+	b->SetIcons(ICON_INFO, 1);
+	appendBanner(b);
+
+	connect(this, SIGNAL(Closed()), SLOT(hide()));
+
+	// create children
+	// . riassunto
+	// . impostazioni IP
+}
