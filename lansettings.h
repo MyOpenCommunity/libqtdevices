c039852f0c73e67eaf0d535ec4c50e4f2f37730d
Gianni Valdambrini <aleister@develer.com>
2008-11-03 18:03:16 +0100

    aggiunte guardie di compilazione

--- lansettings.h
+++ lansettings.h
@@ -1,3 +1,6 @@
+#ifndef LAN_SETTINGS
+#define LAN_SETTINGS
+
 #include "sottomenu.h"
 
 class LanSettings : public sottoMenu
@@ -5,4 +8,8 @@ class LanSettings : public sottoMenu
 Q_OBJECT
 public:
 	LanSettings(QWidget *parent = 0);
+signals:
+	void Closed();
 };
+
+#endif
