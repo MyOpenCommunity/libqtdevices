1a46a9c3d3e0aae20ff1439b7c12993ad641cdd3
Gianni Valdambrini <aleister@develer.com>
2008-11-25 14:43:26 +0100

    aggiunta intestazione

--- automation.h
+++ automation.h
@@ -1,7 +1,21 @@
+/**
+ * \file
+ * <!--
+ * Copyright 2008 Develer S.r.l. (http://www.develer.com/)
+ * All rights reserved.
+ * -->
+ *
+ * \brief This class implements the managing of Automation section
+ *
+ * \author Gianni Valdambrini <aleister@develer.com>
+ * \date November 2008
+ */
+
 #ifndef AUTOMATION_H
 #define AUTOMATION_H
 
 #include "sottomenu.h"
+
 
 class Automation : public sottoMenu
 {
