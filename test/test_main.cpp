836388c55be8cffb3ff6d189da1a06bb49933047
gianni <gianni@xxxpc.(none)>
2009-02-01 16:37:15 +0100

    reso test funzionante (ma ancora da completare)

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,5 +1,4 @@
 #include <landevice.h>
-#include <main.h>
 
 #include <QtTest/QtTest>
 #include <QSignalSpy>
@@ -8,11 +7,7 @@
 #include <QMetaType>
 
 // To put/extract in QVariant
-typedef QHash<int, QVariant> StatusList;
 Q_DECLARE_METATYPE(StatusList)
-
-// To use QHash<> in signal/slots and watch them through QSignalSpy
-int trick = qRegisterMetaType<QHash<int, QVariant> >("QHash<int, QVariant>");
 
 
 class TestLanDevice: public QObject
@@ -27,18 +22,24 @@ private:
 
 void TestLanDevice::initTestCase()
 {
+	// To use StatusList in signal/slots and watch them through QSignalSpy
+	qRegisterMetaType<StatusList>("StatusList");
+
 	device::setClients(0, 0, 0);
 	dev = new LanDevice;
 }
 
 void TestLanDevice::readStatus()
 {
-	QSignalSpy spy(dev, SIGNAL(status_changed(QHash<int, QVariant>)));
+	QSignalSpy spy(dev, SIGNAL(status_changed(StatusList)));
 	dev->frame_rx_handler(const_cast<char*>("*#13**9*0##"));
 	QCOMPARE(spy.count(), 1);
+
 	QVariant signal_arg = spy.takeFirst().at(0);
-	qDebug() << "can convert:" << signal_arg.canConvert<QHash<int, QVariant> >();
-//	StatusList status_list = signal_arg.value<StatusList>();
+	QVERIFY(signal_arg.canConvert<StatusList>());
+	StatusList sl = signal_arg.value<StatusList>();
+	QVERIFY(sl.contains(LanDevice::DIM_STATUS));
+	QVERIFY(sl[LanDevice::DIM_STATUS].toBool() == false);
 }
 
  QTEST_MAIN(TestLanDevice)
