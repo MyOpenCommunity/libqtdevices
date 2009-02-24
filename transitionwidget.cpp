b817041092e5f040774063bfd3072089dadcdafe
Luca Ottaviano <lottaviano@develer.com>
2009-02-24 15:50:16 +0100

    Corretto flickering all'inizio delle transizioni

--- transitionwidget.cpp
+++ transitionwidget.cpp
@@ -13,11 +13,13 @@ TransitionWidget::TransitionWidget(QStackedWidget *win) :
 	connect(&timeline, SIGNAL(valueChanged(qreal)), SLOT(triggerRepaint(qreal)));
 	connect(&timeline, SIGNAL(finished()), &local_loop, SLOT(quit()));
 
-	blending_factor = 1.0;
+	blending_factor = 0.0;
 }
 
 void TransitionWidget::startTransition(Page *prev, Page *next)
 {
+	blending_factor = 0.0;
+	prev_page = QPixmap::grabWidget(prev);
 	// this shows the transition widget (that now shows the prev page)
 	showFullScreen();
 	update();
@@ -26,8 +28,6 @@ void TransitionWidget::startTransition(Page *prev, Page *next)
 	// this sets the next page and applies all layout computation before starting the transition
 	main_window->setCurrentWidget(next);
 	local_loop.processEvents();
-
-	prev_page = QPixmap::grabWidget(prev);
 	next_page = QPixmap::grabWidget(next);
 
 	timeline.start();
