b28b15dcf126f4d5db4fd6daa5d6e25153944a65
Gianni Valdambrini <aleister@develer.com>
2009-02-25 09:52:06 +0100

    refactoring di transitionWidget con l'aggiunta della classe (figlia) per la gestione dell'effetto di transizione a mosaico (ancora non funzionante del tutto)

--- transitionwidget.cpp
+++ transitionwidget.cpp
@@ -1,22 +1,27 @@
 #include "transitionwidget.h"
 #include "page.h"
+#include "main.h"
 
 #include <QPainter>
 #include <QStackedWidget>
 
-#define TRANSITION_TIME 400 // ms
+#include <assert.h>
 
-TransitionWidget::TransitionWidget(QStackedWidget *win) :
-	timeline(TRANSITION_TIME, this)
+
+TransitionWidget::TransitionWidget(QStackedWidget *win, int time) : timeline(time, this)
 {
 	main_window = win;
-	connect(&timeline, SIGNAL(valueChanged(qreal)), SLOT(triggerRepaint(qreal)));
 	connect(&timeline, SIGNAL(finished()), &local_loop, SLOT(quit()));
+}
 
+
+BlendingTransition::BlendingTransition(QStackedWidget *win) : TransitionWidget(win, 400)
+{
+	connect(&timeline, SIGNAL(valueChanged(qreal)), SLOT(triggerRepaint(qreal)));
 	blending_factor = 0.0;
 }
 
-void TransitionWidget::startTransition(Page *prev, Page *next)
+void BlendingTransition::startTransition(Page *prev, Page *next)
 {
 	blending_factor = 0.0;
 	prev_page = QPixmap::grabWidget(prev);
@@ -35,13 +40,13 @@ void TransitionWidget::startTransition(Page *prev, Page *next)
 	hide();
 }
 
-void TransitionWidget::triggerRepaint(qreal value)
+void BlendingTransition::triggerRepaint(qreal value)
 {
 	blending_factor = value;
 	update();
 }
 
-void TransitionWidget::paintEvent(QPaintEvent *e)
+void BlendingTransition::paintEvent(QPaintEvent *e)
 {
 	Q_UNUSED(e);
 	QPainter p(this);
@@ -50,3 +55,59 @@ void TransitionWidget::paintEvent(QPaintEvent *e)
 	p.setOpacity(blending_factor);
 	p.drawPixmap(QPoint(0,0), next_page);
 }
+
+
+MosaicTransition::MosaicTransition(QStackedWidget *win) : TransitionWidget(win, 600)
+{
+	connect(&timeline, SIGNAL(frameChanged(int)), SLOT(triggerRepaint(int)));
+}
+
+void MosaicTransition::startTransition(Page *prev, Page *next)
+{
+	curr_index = 0;
+	const int SQUARE_DIM = 40;
+
+	int num_x = MAX_WIDTH / SQUARE_DIM;
+	int num_y = MAX_HEIGHT / SQUARE_DIM;
+	for (int i = 0; i < num_x * num_y; ++i)
+	{
+		int x = rand() % num_x;
+		int y = rand() % num_y;
+		mosaic_map.append(QRect(x * SQUARE_DIM, y * SQUARE_DIM, SQUARE_DIM, SQUARE_DIM));
+	}
+
+	timeline.setFrameRange(0, mosaic_map.size() - 1);
+	timeline.setStartFrame(0);
+
+	prev_page = QPixmap::grabWidget(prev);
+	// this shows the transition widget (that now shows the prev page)
+	showFullScreen();
+	update();
+	local_loop.processEvents();
+
+	// this sets the next page and applies all layout computation before starting the transition
+	main_window->setCurrentWidget(next);
+	local_loop.processEvents();
+	next_page = QPixmap::grabWidget(next);
+
+	timeline.start();
+	local_loop.exec();
+	hide();
+}
+
+void MosaicTransition::triggerRepaint(int index)
+{
+	assert(index < mosaic_map.size() && "Invalid index value!");
+	curr_index = index;
+	update();
+}
+
+void MosaicTransition::paintEvent(QPaintEvent *e)
+{
+	Q_UNUSED(e);
+	QPainter p(this);
+	p.drawPixmap(QPoint(0,0), prev_page);
+	for (int i = 0; i < curr_index; ++i)
+		p.drawPixmap(mosaic_map.at(i), next_page.copy(mosaic_map.at(i)));
+}
+
--- transitionwidget.h
+++ transitionwidget.h
@@ -4,16 +4,32 @@
 #include <QEventLoop>
 #include <QWidget>
 #include <QTimeLine>
+#include <QList>
+#include <QRect>
 
 class Page;
 class QStackedWidget;
 
-class TransitionWidget : private QWidget
+class TransitionWidget : protected QWidget
 {
 Q_OBJECT
 public:
-	TransitionWidget(QStackedWidget *win);
-	void startTransition(Page *prev, Page *next);
+	TransitionWidget(QStackedWidget *win, int time);
+	virtual void startTransition(Page *prev, Page *next) = 0;
+
+protected:
+	QStackedWidget *main_window;
+	QTimeLine timeline;
+	QEventLoop local_loop;
+};
+
+
+class BlendingTransition : public TransitionWidget
+{
+Q_OBJECT
+public:
+	BlendingTransition(QStackedWidget *win);
+	virtual void startTransition(Page *prev, Page *next);
 
 protected:
 	void paintEvent(QPaintEvent *e);
@@ -22,12 +38,31 @@ private slots:
 	void triggerRepaint(qreal);
 
 private:
-	QTimeLine timeline;
-	QStackedWidget *main_window;
 	QPixmap prev_page;
 	QPixmap next_page;
 	qreal blending_factor;
-	QEventLoop local_loop;
+
+};
+
+
+class MosaicTransition : public TransitionWidget
+{
+Q_OBJECT
+public:
+	MosaicTransition(QStackedWidget *win);
+	virtual void startTransition(Page *prev, Page *next);
+
+protected:
+	void paintEvent(QPaintEvent *e);
+
+private slots:
+	void triggerRepaint(int index);
+
+private:
+	int curr_index;
+	QList<QRect> mosaic_map;
+	QPixmap prev_page;
+	QPixmap next_page;
 };
 
 #endif // TRANSITIONWIDGET_H
