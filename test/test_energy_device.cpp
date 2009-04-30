0b1a798f92fd6c02de591124b56ee572b4702a77
Gianni Valdambrini <aleister@develer.com>
2009-04-30 14:32:25 +0200

    ripristinata correttezza test

--- test/test_energy_device.cpp
+++ test/test_energy_device.cpp
@@ -178,7 +178,7 @@ void TestEnergyDevice::receiveDailyAverageGraph2()
 	dev->buffer_frame.clear();
 	DeviceTester t(dev, EnergyDevice::DIM_DAILY_AVERAGE_GRAPH);
 	QStringList frames;
-	frames << "*#18*20*57#9*1*2*5##" << "*#18*20*57#9*2*255*9*3##";
+	frames << "*#18*20*57#9*1*2*5##" << "*#18*20*57#9*2*0*9*3##";
 	QVariant result = t.getResult(frames);
 
 	QVERIFY(result.canConvert<GraphData>());
@@ -349,7 +349,7 @@ void TestEnergyDevice::receiveCumulativeMonthGraph()
 	QVERIFY(result.canConvert<GraphData>());
 
 	GraphData data;
-	data.graph[1] = (3 << 8) + 0;
+	data.graph[1] = (3 << 8) + 255;
 	data.date = QDate(2008, 8, 1);
 	data.type = EnergyDevice::CUMULATIVE_MONTH;
 	QVERIFY(data == result.value<GraphData>());
@@ -365,15 +365,15 @@ void TestEnergyDevice::receiveCumulativeMonthGraph2()
 
 	frames << tmp + "*1*3*255##"
 		<< tmp + "*2*20*90*255##"
-		<< tmp + "*3*30*0*45##"
+		<< tmp + "*3*255*0*45##"
 		<< tmp + "*4*45*150*185##";
 	QVariant result = t.getResult(frames);
 	QVERIFY(result.canConvert<GraphData>());
 
 	GraphData data;
-	data.graph[1] = (3 << 8) + 0;
+	data.graph[1] = (3 << 8) + 255;
 	data.graph[2] = (20 << 8) + 90;
-	data.graph[3] = 30;
+	data.graph[3] = 0;
 	data.graph[4] = 45;
 	data.graph[5] = (45 << 8) + 150;
 	data.date = QDate(2008, 8, 1);
