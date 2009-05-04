bb73f2666a485371fa28869c5eb37bc4ecb454e2
Luca Ottaviano <lottaviano@develer.com>
2009-05-04 12:46:56 +0200

    Fix bug #202, navigazione grafici

--- energy_view.cpp
+++ energy_view.cpp
@@ -598,7 +598,8 @@ void EnergyView::changeTimePeriod(int status, QDate selection_date)
 		break;
 	case TimePeriodSelection::MONTH:
 		// we have to preserve the current visualized graph (can be daily average)
-		graph_type = current_graph;
+		if (current_graph != EnergyDevice::DAILY_AVERAGE)
+			graph_type = EnergyDevice::CUMULATIVE_MONTH;
 		dev->requestCumulativeMonth(selection_date);
 		dev->requestCumulativeMonthGraph(selection_date);
 		dev->requestDailyAverageGraph(selection_date);
