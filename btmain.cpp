51014250855eaba5a35b11c4493f160443f86eda
Simone Agresta <simone.agresta@bticino.it>
2009-06-19 17:27:29 +0200

    Gestione aggiornamenti eth
    	Con questa modifica la schermata che indica che c'è una configurazione o un aggiornamento in corso comparirà anche in caso di procedura via ETH e non solo via USB/SERIALE.

--- btmain.cpp
+++ btmain.cpp
@@ -341,7 +341,7 @@ void BtMain::testFiles()
 			tempo1->stop();
 		}
 	}
-	else if (QFile::exists(FILE_AGGIORNAMENTO))
+	else if ((QFile::exists(FILE_AGGIORNAMENTO)) || (QFile::exists(FILE_AGGIORNAMENTO_ETH)))
 	{
 		if ((screen) && (tiposcreen != genPage::IMAGE))
 		{
