fc98eeba7f52e771fffe1c933b43050c68e68ef3
Gianni Valdambrini <aleister@develer.com>
2009-09-24 18:13:33 +0200

    gestito cambio pagina durante lo screensaver per gli allarmi con soluzione da rivedere (ticket #266)

--- antintrusion.cpp
+++ antintrusion.cpp
@@ -287,6 +287,8 @@ void Antintrusion::gesFrame(char*frame)
 		if (!previous_page)
 			previous_page = currentPage();
 		allarme *curr = allarmi.at(curr_alarm);
+		// if the alarm arrive during the screensaver, we want to turn back to the alarm when the screensaver exit
+		bt_global::btmain->setPreviousPage(curr);
 		curr->showPage();
 		ctrlAllarm();
 	}
