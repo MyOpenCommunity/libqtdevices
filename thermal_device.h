9271c46fa6c70d3cb90b989fb121b7f4116f1f13
Luca Ottaviano <lottaviano@develer.com>
2009-11-09 10:09:37 +0100

    Spostato un enum (protected) nel file di implementazione
    I valori non hanno interesse fuori dalla classe, meglio eliminare
    dettagli non utili dall'interfaccia.

--- thermal_device.h
+++ thermal_device.h
@@ -82,50 +82,6 @@ public:
 	virtual thermo_type_t type() const = 0;
 
 protected:
-	enum what_t
-	{
-		SUMMER = 0,
-		WINTER = 1,
-		GENERIC_MODE = 3,
-		TEMPERATURE_SET = 14,            // set the temperature in manual operation, this is a dimension
-		HOLIDAY_DATE_END = 30,           // set the end date of holiday mode, this is a dimension (grandezza)
-		HOLIDAY_TIME_END = 31,           // set the end time of holiday mode, this is a dimension (grandezza)
-		MANUAL_TIMED_END = 32,           // set the end time of timed manual mode
-
-		// summer specific identifiers
-		SUM_PROTECTION = 202,            // protection
-		SUM_OFF = 203,                   // off
-		SUM_MANUAL = 210,                // manual operation (all zones in setpoint temperature)
-		SUM_MANUAL_TIMED = 212,          // manual operation (24h maximum)
-		SUM_WEEKEND = 215,               // weekend operation (festivo)
-		SUM_PROGRAM = 2100,              // weekly program (1 out of 3)
-		SUM_SCENARIO = 2200,             // scenario (1 out of 16, 99zones thermal regulator only)
-		SUM_HOLIDAY = 23000,             // holiday operation (programma ferie)
-
-		// winter specific identifiers
-		WIN_PROTECTION = 102,
-		WIN_OFF = 103,                   // off
-		WIN_MANUAL = 110,                // manual operation (all zones in setpoint temperature)
-		WIN_MANUAL_TIMED = 112,          // manual operation (24h maximum)
-		WIN_WEEKEND = 115,               // weekend operation (festivo)
-		WIN_PROGRAM = 1100,              // weekly program (1 out of 3)
-		WIN_SCENARIO = 1200,             // scenario (1 out of 16, 99zones thermal regulator only)
-		WIN_HOLIDAY = 13000,             // holiday operation (programma ferie)
-
-		// generic identifiers (useful for issuing commands)
-		GENERIC_PROTECTION = 302,
-		GENERIC_OFF = 303,
-		GENERIC_MANUAL_TIMED = 312,      // timed manual operation (generic mode)
-		GENERIC_WEEKEND = 315,           // command to set weekend mode
-		WEEK_PROGRAM = 3100,             // command to set the program to be executed (generic mode)
-						 // remember to add the program number to this number
-		SCENARIO_PROGRAM = 3200,         // command to set the scenario to be executed (generic mode)
-						 // remember to add the program number to this number
-		HOLIDAY_NUM_DAYS = 33000,        // command to set the number of days of holiday mode (generic mode)
-						 // remember to add the number of days to this number
-	};
-
-protected:
 	ThermalDevice(QString where);
 
 	void sendWriteRequest(const QString &what);
