f2681abb12702920018e51c25904e49e8a4f04a6
Mattia Barbon <mattia@develer.com>
2009-11-10 10:51:22 +0100

    Rimosso page layout dalla pagina della sorgente Aux.

--- aux.cpp
+++ aux.cpp
@@ -2,6 +2,7 @@
 #include "btbutton.h"
 #include "main.h" // ICON_FFWD
 #include "fontmanager.h" // bt_global::font
+#include "navigation_bar.h"
 
 #include <QVBoxLayout>
 #include <QLabel>
@@ -9,8 +10,14 @@
 
 Aux::Aux(const QString &name, const QString &amb)
 {
+	QWidget *content = new QWidget;
+	QVBoxLayout *main_layout = new QVBoxLayout(content);
+
+	main_layout->setContentsMargins(0, 0, 0, 0);
+	main_layout->setSpacing(0);
+
 	main_layout->addSpacing(30);
-	aux_name = new QLabel;
+	QLabel *aux_name = new QLabel;
 	aux_name->setFont(bt_global::font->get(FontManager::SUBTITLE));
 	aux_name->setText(name);
 	main_layout->addWidget(aux_name, 0, Qt::AlignHCenter);
@@ -22,11 +29,16 @@ Aux::Aux(const QString &name, const QString &amb)
 	main_layout->addWidget(amb_descr, 0, Qt::AlignHCenter);
 
 	main_layout->addSpacing(20);
-	fwd_but = new BtButton;
+	BtButton *fwd_but = new BtButton;
 	fwd_but->setImage(ICON_FFWD);
 	main_layout->addWidget(fwd_but, 0, Qt::AlignHCenter);
+	main_layout->addStretch(1);
 
-	addBackButton();
+	NavigationBar *nav_bar = new NavigationBar;
+	nav_bar->displayScrollButtons(false);
+	buildPage(content, nav_bar);
+
+	connect(nav_bar, SIGNAL(backClick()), SIGNAL(Closed()));
 	connect(fwd_but, SIGNAL(clicked()), SIGNAL(Btnfwd()));
 }
 
