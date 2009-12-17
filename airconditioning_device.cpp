8b14d0f0a07697ba9bdc183c5fb528abd86c2aca
Gianni Valdambrini <aleister@develer.com>
2009-12-17 17:00:23 +0100

    abbozzato device advanced split (aria condizionata)

--- airconditioning_device.cpp
+++ airconditioning_device.cpp
@@ -1,4 +1,10 @@
 #include "airconditioning_device.h"
+#include "generic_functions.h" // createWriteRequestOpen
+
+#include <openmsg.h>
+
+
+#define ADVANCED_SPLIT_DIM 22
 
 
 AirConditioningDevice::AirConditioningDevice(QString where) : device("0", where)
@@ -21,3 +27,45 @@ void AirConditioningDevice::setOffCommand(QString off_cmd)
 	off = off_cmd;
 }
 
+
+
+AdvancedAirConditioningDevice::AdvancedAirConditioningDevice(QString where) : device("4", where)
+{
+}
+
+void AdvancedAirConditioningDevice::requestStatus() const
+{
+	sendRequest(QString::number(ADVANCED_SPLIT_DIM));
+}
+
+void AdvancedAirConditioningDevice::setStatus(Mode mode, int temp, Velocity vel, Swing swing)
+{
+	QString what;
+
+	switch (mode)
+	{
+	case MODE_OFF:
+		what = QString("%1*%2***").arg(ADVANCED_SPLIT_DIM).arg(mode);
+		break;
+
+	case MODE_FAN:
+	case MODE_DEHUM:
+		what = QString("%1*%2**%3*%4").arg(ADVANCED_SPLIT_DIM).arg(mode).arg(vel).arg(swing);
+		break;
+
+	default:
+		what = QString("%1*%2*%3*%4*%5").arg(ADVANCED_SPLIT_DIM).arg(mode).arg(temp).arg(vel).arg(swing);
+		break;
+	}
+
+	sendFrame(createWriteRequestOpen(who, what, where));
+}
+
+void AdvancedAirConditioningDevice::frame_rx_handler(char *frame)
+{
+	OpenMsg msg;
+	msg.CreateMsgOpen(frame, strlen(frame));
+
+	if (who.toInt() != msg.who() || msg.where() != where.toInt())
+		return;
+}
--- airconditioning_device.h
+++ airconditioning_device.h
@@ -20,4 +20,47 @@ private:
 	QString off;
 };
 
+
+
+class AdvancedAirConditioningDevice : public device
+{
+Q_OBJECT
+public:
+	AdvancedAirConditioningDevice(QString where);
+
+
+	enum Mode
+	{
+		MODE_OFF = 0,
+		MODE_WINTER = 1,
+		MODE_SUMMER = 2,
+		MODE_FAN = 3,
+		MODE_DEHUM = 4,
+		MODE_AUTO = 5
+	};
+
+	enum Velocity
+	{
+		VEL_AUTO = 0,
+		VEL_MIN = 1,
+		VEL_MED = 2,
+		VEL_MAX = 3,
+		VEL_SILENT = 4
+	};
+
+	enum Swing
+	{
+		SWING_OFF = 0,
+		SWING_ON = 1
+	};
+
+	void requestStatus() const;
+
+	void setStatus(Mode mode, int temp, Velocity vel, Swing swing);
+
+public slots:
+	//! receive a frame
+	void frame_rx_handler(char *frame);
+};
+
 #endif // AIRCONDITIONING_DEVICE_H
