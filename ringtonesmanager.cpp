0f610ef4b10ebb9d5634c3b1f726a127b339665d
Luca Ottaviano <lottaviano@develer.com>
2010-01-15 17:42:42 +0100

    Prima bozza di implementazione delle suonerie.
    Le suonerie sono un concetto globale, necessitano di un oggetto globale
    che le gestisca.
    Ci sono due livelli di mappatura:
     - indicizzazione dei file presenti su disco, tramite la mappa
       ringtones_to_file;
     - mappa tra funzionalità e indice, tramite type_to_ringtone.
    
    Per funzionalità si intende il tipo di suoneria, sia esso suoneria 1 per
    la vct, suoneria per allarmi o per intercom.
    La classe fornisce metodi per cambiare l'associazione fra funzionalità e
    suoneria; la mappa ringtones_to_file è per uso interno e non deve essere
    modificata dall'utente.
    Permette anche di suonare una suoneria tramite il metodo playRingtone().

---
+++ ringtonesmanager.cpp
@@ -0,0 +1,66 @@
+#include "ringtonesmanager.h"
+#include "hardware_functions.h"
+
+#include <QStringList>
+#include <QDebug>
+#include <QDir>
+
+// TODO: this is not absolute for testing
+#define RINGTONE_DIR "cfg/extra/2/"
+
+RingtonesManager::RingtonesManager()
+{
+	QStringList filters;
+	filters << "*.[wW][[aA][vV]";
+	int id = 0;
+	QDir d(RINGTONE_DIR);
+	foreach (const QFileInfo &fi, scanDirectory(d, filters))
+	{
+		ringtone_to_file[id] = fi.filePath();
+		qDebug() << fi.filePath();
+		++id;
+	}
+	// TODO: read the map type_to_ringtone saved on flash memory
+
+	qDebug() << "Found the following ringtones: " << ringtone_to_file;
+}
+
+void RingtonesManager::playRingtone(RingtoneType t)
+{
+	// TODO: maybe we will need a stopRingtone too
+	playSound(typeToFilePath(t));
+}
+
+void RingtonesManager::setRingtone(RingtoneType t, int ring)
+{
+	Q_ASSERT_X(ring >= 0 && ring < ringtone_to_file.size(), "RingtonesManager::setRingtone",
+		"Given ringtone is outside valid range.");
+	type_to_ringtone[t] = ring;
+	// TODO: save the map to flash memory
+}
+
+int RingtonesManager::getRingtonesNumber()
+{
+	return ringtone_to_file.size();
+}
+
+QString RingtonesManager::typeToFilePath(RingtoneType t)
+{
+	return ringtone_to_file[type_to_ringtone[t]];
+}
+
+// TODO: this can be shared with AudioFileSelector::browseFiles()?
+QList<QFileInfo> RingtonesManager::scanDirectory(const QDir &dir, const QStringList &filters)
+{
+	QList<QFileInfo> temp_files_list;
+
+	foreach (const QFileInfo &fi, dir.entryInfoList(filters))
+	{
+		if (fi.fileName() != "." && fi.fileName() != "..")
+			temp_files_list.append(fi);
+	}
+
+	return temp_files_list;
+}
+
+RingtonesManager *bt_global::ringtones = 0;
