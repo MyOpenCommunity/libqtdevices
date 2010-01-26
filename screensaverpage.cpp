287834406676c9c749ad85870d3fbe6bed4a8d27
Luca Ottaviano <lottaviano@develer.com>
2010-01-26 12:26:01 +0100

    Prima bozza di selezione foto per screensaver slideshow

--- screensaverpage.cpp
+++ screensaverpage.cpp
@@ -3,8 +3,12 @@
 #include "displaycontrol.h" // bt_global::display
 #include "hardware_functions.h"
 #include "singlechoicecontent.h"
+#include "navigation_bar.h" // NavigationBar
+#include "skinmanager.h" // bt_global::skin
 
 #include <QAbstractButton>
+#include <QGridLayout>
+#include <QLabel>
 
 ScreenSaverPage::ScreenSaverPage()
 {
@@ -41,3 +45,118 @@ void ScreenSaverPage::bannerSelected(int id)
 	if (id == ScreenSaver::NONE)
 		bt_global::display.setBrightness(BRIGHTNESS_OFF);
 }
+
+
+
+SlideshowImageContent::SlideshowImageContent(QWidget *parent) :
+	GridContent(parent)
+{
+
+}
+
+void SlideshowImageContent::addItem(QWidget *item)
+{
+	items.append(item);
+}
+
+void SlideshowImageContent::drawContent()
+{
+	QGridLayout *l = qobject_cast<QGridLayout*>(layout());
+
+	if (pages.size() == 0)
+	{
+		// compute the page list
+		prepareLayout(items, 2);
+
+		// add icons to the layout
+		for (int i = 0; i < pages.size() - 1; ++i)
+		{
+			int base = pages[i];
+			for (int j = 0; base + j < pages[i + 1]; ++j)
+				l->addWidget(items.at(base + j), j / 2, j % 2);
+		}
+
+		l->setRowStretch(l->rowCount(), 1);
+	}
+
+	updateLayout(items);
+}
+
+void SlideshowImageContent::nextItem()
+{
+	current_page += 1;
+	// wrap around to the first page
+	if (current_page >= pageCount())
+		current_page = 0;
+	drawContent();
+}
+
+void SlideshowImageContent::prevItem()
+{
+	current_page -= 1;
+	// wrap around to the last page
+	if (current_page < 0)
+		current_page = pageCount() - 1;
+	drawContent();
+}
+
+
+
+SlideshowSelectionPage::SlideshowSelectionPage(const QString &start_path) :
+	Page(0),
+	current_dir(start_path),
+	level(0)
+{
+	NavigationBar *nav_bar = new NavigationBar;
+	buildPage(new SlideshowImageContent, nav_bar, "Select photos");
+	connect(nav_bar, SIGNAL(backClick()), SLOT(browseUp()));
+	connect(nav_bar, SIGNAL(upClick()), page_content, SLOT(prevItem()));
+	connect(nav_bar, SIGNAL(downClick()), page_content, SLOT(nextItem()));
+	connect(page_content, SIGNAL(displayScrollButtons(bool)), nav_bar, SLOT(displayScrollButtons(bool)));
+
+	checked_icon = bt_global::skin->getImage("checked");
+	unchecked_icon = bt_global::skin->getImage("unchecked");
+	forward_icon = bt_global::skin->getImage("forward");
+
+	current_dir.setSorting(QDir::DirsFirst | QDir::Name);
+	current_dir.setFilter(QDir::AllDirs | QDir::Files | QDir::NoSymLinks | QDir::NoDotAndDotDot | QDir::Readable);
+	showFiles();
+}
+
+void SlideshowSelectionPage::showFiles()
+{
+	QStringList filters;
+	filters << "*.jpg" << "*.png";
+
+	QFileInfoList list = current_dir.entryInfoList(filters);
+	foreach (const QFileInfo &fi, list)
+	{
+		QWidget *w = 0;
+		if (fi.isDir())
+			w = new QLabel(fi.fileName());
+
+		if (fi.isFile())
+		{
+			QPixmap tmp(fi.filePath());
+			tmp = tmp.scaled(QSize(100, 100), Qt::KeepAspectRatio);
+			QLabel *l = new QLabel;
+			l->setPixmap(tmp);
+			w = l;
+		}
+		Q_ASSERT_X(w!=0, "SlideshowImageSelection::browseFiles", "w is 0");
+		page_content->addItem(w);
+	}
+}
+
+void SlideshowSelectionPage::browseUp()
+{
+	if (level > 0)
+	{
+		--level;
+		current_dir.cdUp();
+		showFiles();
+	}
+	else
+		emit Closed();
+}
+
--- screensaverpage.h
+++ screensaverpage.h
@@ -2,6 +2,9 @@
 #define SCREENSAVERPAGE_H
 
 #include "singlechoicepage.h"
+#include "gridcontent.h" // GridContent
+
+#include <QDir>
 
 
 class ScreenSaverPage : public SingleChoicePage
@@ -14,6 +17,45 @@ public:
 protected:
 	virtual int getCurrentId();
 	virtual void bannerSelected(int id);
+};
+
+class SlideshowImageContent;
+
+class SlideshowSelectionPage : public Page
+{
+Q_OBJECT
+public:
+	typedef SlideshowImageContent ContentType;
+
+	SlideshowSelectionPage(const QString &start_path);
+
+public slots:
+	void browseUp();
+
+private:
+	void showFiles();
+	QDir current_dir;
+	int level;
+	QString checked_icon, unchecked_icon, forward_icon;
+};
+
+
+class SlideshowImageContent : public GridContent
+{
+Q_OBJECT
+public:
+	SlideshowImageContent(QWidget *parent=0);
+	void addItem(QWidget *item);
+
+public slots:
+	void nextItem();
+	void prevItem();
+
+protected:
+	virtual void drawContent();
+
+private:
+	QList<QWidget *> items;
 };
 
 #endif // SCREENSAVERPAGE_H
