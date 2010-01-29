b37698a2c86091d0b7634e490513d4852381c087
Mattia Barbon <mattia@develer.com>
2010-01-29 15:47:43 +0100

    Spostati gli script per installare gli eseguibili nella stessa cartella degli eseguibili.

--- install_touchx.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#sudo rm -f $HOME/src/nfs_touchx/etc/pointercal
-cp touchx/TouchX.touchx.arm /nfs_touchx/home/bticino/bin/btmmt
---
+++ touchx/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#sudo rm -f $HOME/src/nfs_touchx/etc/pointercal
+cp TouchX.arm /nfs_touchx/home/bticino/bin/btmmt
