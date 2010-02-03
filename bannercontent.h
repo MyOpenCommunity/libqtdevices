d93af1c7aa5d7fa53e43d029c9b167a72dd76f71
Luca Ottaviano <lottaviano@develer.com>
2010-02-03 16:45:57 +0100

    Aggiornato BannerContent per usare paginazione non ciclica
    Il codice è praticamente tutto ripreso da GridContent nel ramo master.
    Sperabilmente potremo usare lo stesso codice quando si fa il merge nel
    master.

--- bannercontent.h
+++ bannercontent.h
@@ -37,9 +37,7 @@ signals:
 	void contentScrolled(int current, int total);
 };
 
-/**
- * The BannerContent class manage a list of banner usually put inside a Page.
- */
+
 class BannerContent : public BannerContentBase
 {
 Q_OBJECT
@@ -52,22 +50,16 @@ public slots:
 	virtual void resetIndex();
 
 protected:
-	void showEvent(QShowEvent *e);
 	virtual void drawContent();
 
 private:
 	int pageCount() const;
-	int calculateNextIndex(bool up_to_down);
+	void prepareLayout(QList<QWidget *> items, int columns);
+	void updateLayout(QList<QWidget *> items);
 
-	bool need_update; // a flag to avoid useless call to drawContent
-
-	// The index of the banner showed at the top of the BannerContent
-	int current_index;
-
-	bool first_time; // true if the page is shown for the first time
-
-	// true if the banners height exceed the BannerContent height (so we have to do a pagination)
-	bool need_pagination;
+	bool need_update;
+	int current_page;
+	QList<int> pages;
 };
 
 #endif // CONTENT_WIDGET_H
