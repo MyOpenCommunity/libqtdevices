68d2c8757fd174ba20518af12c79119015d4bcc8
Luca Ottaviano <lottaviano@develer.com>
2010-02-08 16:18:57 +0100

    Aggiunto parsing per banner con centralina, chiarite alcune label

--- load_management.cpp
+++ load_management.cpp
@@ -8,6 +8,7 @@
 #include "navigation_bar.h" //NavigationBar
 
 #include <QLabel>
+#include <QDebug>
 #include <QVBoxLayout>
 
 LoadManagement::LoadManagement(const QDomNode &config_node) :
@@ -35,15 +36,28 @@ banner *LoadManagement::getBanner(const QDomNode &item_node)
 	SkinContext context(getTextChild(item_node, "cid").toInt());
 	int id = getTextChild(item_node, "id").toInt();
 	QString where = getTextChild(item_node, "where");
+	int pos = where.indexOf('#');
+	Q_ASSERT_X(pos > -1, "LoadManagement::getBanner", "Device where must have a '#' character");
+	QString descr = getTextChild(item_node, "descr");
+	// remove part after '#'
+	QString priority = where.left(pos);
+	// remove first 7
+	priority = priority.mid(1);
 
 	banner *b = 0;
 	switch (id)
 	{
 	case LOAD_WITH_CU:
+	{
+		bool advanced = getTextChild(item_node, "advanced").toInt();
+		BannLoadWithCU *bann = new BannLoadWithCU(priority + ". " + descr, advanced ? BannLoadWithCU::ADVANCED_MODE : BannLoadWithCU::BASE_MODE);
+		// TODO: connect to next page if advanced
+		b = bann;
+	}
 		break;
 	case LOAD_WITHOUT_CU:
 	{
-		BannLoadNoCU *bann = new BannLoadNoCU(getTextChild(item_node, "descr"));
+		BannLoadNoCU *bann = new BannLoadNoCU(descr);
 		Page *p = new LoadDataPage(item_node);
 		bann->connectRightButton(p);
 		connect(p, SIGNAL(Closed()), bann, SIGNAL(pageClosed()));
@@ -62,21 +76,21 @@ banner *LoadManagement::getBanner(const QDomNode &item_node)
 LoadDataContent::LoadDataContent()
 {
 	current_consumption = new QLabel;
-	current_consumption->setText("ciao ciao");
+	current_consumption->setText("Current consumption");
 	current_consumption->setFont(bt_global::font->get(FontManager::SUBTITLE));
 
 	BannSinglePuls *first_period = new BannSinglePuls(0);
 	first_period->initBanner(bt_global::skin->getImage("ok"), bt_global::skin->getImage("empty_background"), "data/ora del reset");
-	first_period->setCentralText("TESTO AL CENTRO");
+	first_period->setCentralText("Total consumption 1");
 	connect(first_period, SIGNAL(rightClick()), SIGNAL(firstReset()));
 	BannSinglePuls *second_period = new BannSinglePuls(0);
 	second_period->initBanner(bt_global::skin->getImage("ok"), bt_global::skin->getImage("empty_background"), "data/ora del reset");
-	second_period->setCentralText("TESTO AL CENTRO");
+	second_period->setCentralText("Total consumption 2");
 	connect(second_period, SIGNAL(rightClick()), SIGNAL(secondReset()));
 	QVBoxLayout *main = new QVBoxLayout(this);
 	main->setContentsMargins(0, 0, 0, 0);
 	main->setSpacing(0);
-	main->addWidget(current_consumption);
+	main->addWidget(current_consumption, 0, Qt::AlignHCenter);
 	main->addWidget(first_period);
 	main->addWidget(second_period);
 }
@@ -97,6 +111,7 @@ LoadDataPage::LoadDataPage(const QDomNode &config_node)
 	page_title->setFont(bt_global::font->get(FontManager::TEXT));
 
 	NavigationBar *nav_bar = new NavigationBar;
+	nav_bar->displayScrollButtons(false);
 	connect(nav_bar, SIGNAL(backClick()), SIGNAL(Closed()));
 	QVBoxLayout *main = new QVBoxLayout(this);
 	main->setContentsMargins(0, 5, 0, 10);
