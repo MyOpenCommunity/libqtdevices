2ecf016619aa81fc3f21b55f60158f3660c2d934
Luca Ottaviano <lottaviano@develer.com>
2010-02-23 13:02:49 +0100

    Cambiata la nota di copyright nei sorgenti dell'interfaccia

--- bann4_buttons.h
+++ bann4_buttons.h
@@ -1,12 +1,23 @@
-/****************************************************************
-**
-** BTicino Touch scren Colori art. H4686
-**
-** bann4_buttons.h
-**
-** In this file there are contained all generic banners with 4 buttons.
-**
-****************************************************************/
+/* 
+ * BTouch - Graphical User Interface to control MyHome System
+ *
+ * Copyright (C) 2010 BTicino S.p.A.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 
 #ifndef BANN4_BUTTONS_H
 #define BANN4_BUTTONS_H
