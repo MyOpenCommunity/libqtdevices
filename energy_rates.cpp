0dc885fc94a98727d69223d17d5da8a585447fd5
Mattia Barbon <mattia@develer.com>
2010-03-31 15:09:15 +0200

    Bug #2000: Permette di caricare un file di configurazione senza valutazione economica per l'energy.

--- energy_rates.cpp
+++ energy_rates.cpp
@@ -55,9 +55,6 @@ void EnergyRates::loadRates()
 
 		rates[rate.id] = rate;
 	}
-
-	Q_ASSERT_X(!rates.isEmpty(), "EnergyRates::loadRates",
-		   "Energy management configured but no rates defined");
 }
 
 EnergyRate EnergyRates::getRate(int rate_id) const
