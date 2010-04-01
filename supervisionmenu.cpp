e70c7b3173902a0344d755134056abad8a580ae3
Mattia Barbon <mattia@develer.com>
2010-04-01 17:13:08 +0200

    Bug #1723: Nella pagina del load diagnostic va fatta una richiesta di livello, non di stato.

--- supervisionmenu.cpp
+++ supervisionmenu.cpp
@@ -152,5 +152,5 @@ LoadDiagnosticPage::LoadDiagnosticPage(const QDomNode &config_node)
 void LoadDiagnosticPage::showEvent(QShowEvent *)
 {
 	foreach (const LoadsDevice *d, devices)
-		d->requestStatus();
+		d->requestLevel();
 }
