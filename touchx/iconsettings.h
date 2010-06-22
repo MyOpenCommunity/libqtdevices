ae1e51245b86b55f768082f42ce96405e26b2c90
Mattia Barbon <mattia@develer.com>
2010-06-22 16:52:04 +0200

    Nella pagina del volume, aspetta che termini il cambio di stato della macchina a stati per l'audio prima di riprodurre la suoneria.

--- touchx/iconsettings.h
+++ touchx/iconsettings.h
@@ -86,16 +86,14 @@ Q_OBJECT
 public:
 	VolumePage(const QDomNode &config_node);
 
-public slots:
-	void changeVolume(int new_vol);
-	virtual void showPage();
-	virtual void cleanUp();
-
 private slots:
-	void handleClose();
+	void ringtoneFinished();
+	void playRingtone();
+	void startPlayRingtone();
 
 private:
 	ItemTuning *volume;
+	bool is_playing;
 };
 
 
