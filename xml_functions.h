18766f217f1c15517d132aea9a7342893a1b48e7
Lorenzo Masini <rugginoso@develer.com>
2010-10-13 11:49:48 +0200

    Added documentation for the xml functions.

--- xml_functions.h
+++ xml_functions.h
@@ -27,41 +27,18 @@
 #include <QString>
 
 
-/// Return the node child of 'parent' with name 'name'.
 QDomNode getChildWithName(const QDomNode &parent, const QString &name);
 
-/**
- * Finds a node in a DOM tree with the path given as a parameter.
- * \param root the node root of the tree.
- * \param path The path in the xml tree that leads to the node. The path must omit the root node
- * (example: node "configuratore" in conf.xml) and must separate the nodes with a '/'. The last node
- * must not be followed by a '/'.
- * Example: "setup/generale/clock/dateformat"
- * \return A Null node if the path is not found, a valid node otherwise.
- */
 QDomElement getElement(const QDomNode &root, const QString &path);
 
-/// Return the node child of 'parent' that match the 'regexp' and have an attribute with id 'id'.
 QDomNode getChildWithId(const QDomNode &parent, const QRegExp &node_regexp, int id);
 
-/// Return the node child of 'parent' that match the 'regexp' and has a child with name idName and value=id.
 QDomNode getChildWithId(const QDomNode &parent, const QRegExp &node_regexp, const QString &idName, int id);
 
-/// A wrapper function around getChildWithName. Return the text of child if present, a null QString otherwise.
 QString getTextChild(const QDomNode &parent, const QString &name);
 
-/// Return a list of children of node 'parent' whose name start with 'name'.
 QList<QDomNode> getChildren(const QDomNode &parent, const QString &name);
 
-/**
-  * Find a node in the DOM tree that is descendent of the node 'root' and have a child "tag_name" with a "value".
-  * \param root the node root of the tree
-  * \param node_regexp the regular expression that has to be matched with the target node
-  * \param tag_name the name of the tag to search
-  * \param value the value of the target node
-  * \param the serial number of the target node.
-  * \return A Null node if the node is not found, the target node ortherwise.
-  */
 QDomNode findXmlNode(const QDomNode &root, const QRegExp &node_regexp, const QString &tag_name, int id, int& serial_number);
 
 inline QDomNode findXmlNode(const QDomNode &root, const QRegExp &node_regexp, const QString &tag_name, int id)
