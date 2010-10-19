bb8ff5ec410cb1c487aeb92a49d0da4a11bf43e2
Lorenzo Masini <rugginoso@develer.com>
2010-10-19 13:00:39 +0200

    Aggiunta documentazione per la class ItemList.

--- itemlist.h
+++ itemlist.h
@@ -34,16 +34,38 @@ class QVBoxLayout;
 class QBoxLayout;
 class QLabel;
 
-/**
- * \class ItemList
- *
- * This class show a list of item (files or other "virtual" items) and supply a
- * list of signal/slot for handling the user choices.
+/*!
+	\ingroup Core
+	\brief Class to show a list of items
+
+	This class show a list of items (like files or other "virtual" items) and
+	supply a list of signal/slot for handling the user choices.
+
+	It works like the ScrollableContent class, but it can display only homogeneus
+	items.
+
+	The structure to represent a single item is the ItemList::ItemInfo structure.
+	You can set a list of this structure to the ItemList using the setList()
+	method. You can insert a single item, too, using the inserItem() method.
+	Items can be removed one a time using the removeItem() method or all together
+	using the clear() method.
+	To retrive or access directly an item, you can use the item() method.
+	The total number of items contained into the ItemList can be taked using the
+	itemCount() method.
+
+	To customize the look&feel of the ItemInfo rappresentation on the ItemList,
+	you can reimplement the addHorizontalBanner() method.
  */
 class ItemList : public QWidget
 {
 Q_OBJECT
 public:
+	/*!
+		\brief Structure to rappresent an item.
+
+		An item can have a name, a description a list of icons.
+		It has a QVariant field to store any kind of data the user wants.
+	*/
 	struct ItemInfo
 	{
 		ItemInfo(QString name = QString(), QString desc = QString(),
@@ -55,57 +77,142 @@ public:
 		QVariant data;
 	};
 
+	/*!
+		\brief Constructor.
+
+		Construct a new ItemList with the given \a parent and arranging the
+		items in \a rows_per_page rows.
+	*/
 	ItemList(QWidget *parent, int rows_per_page);
 
+	/*!
+		\brief Sets the list of items.
+
+		Sets \a items to be the content of the item list, and switch to the page
+		\a page (the number of pages is calculated using the
+		ItemList::rows_per_page member).
+	*/
 	void setList(QList<ItemInfo> items, int page = 0);
+
+	/*!
+		\brief Shows the subset of items of the current page.
+	*/
 	void showList();
+
+	/*!
+		\brief Returns the reference to the ItemInfo structure of the
+		item \a index.
+	*/
 	ItemInfo &item(int index);
+
+	/*!
+		\brief Returns the total count of items contained into the ItemList.
+	*/
 	int itemCount();
 
-	/**
-	 * Inserts \a item at index position \a index in the list moving
-	 * the successive items forward.
-	 * If \a index is 0, the \a item is prepended to the list.
-	 * If \a index is \a itemCount(), the value is appended to the list.
+	/*!
+		\brief Inserts \a item at position \a index.
+
+		The successive items are moved forward.
+		If \a index is 0, the \a item is prepended to the list.
+		If \a index is \a itemCount(), the value is appended to the list.
 	 */
 	void insertItem(int index, const ItemInfo &item);
+
+	/*!
+		\brief Removes the item at position \a index from the ItemList.
+	*/
 	void removeItem(int index);
 
+	/*!
+		\brief Returns the current page index.
+	*/
 	unsigned getCurrentPage();
 
 signals:
+	/*!
+		\brief Emitted when an item is clicked.
+	*/
 	void itemIsClicked(int item);
+
+	/*!
+		\brief Emitted when the layout is updated.
+
+		\a display is true if scrolling is possible, false otherwise.
+	*/
 	void displayScrollButtons(bool);
-	void contentScrolled(int, int);
+
+	/*!
+		\brief Emitted when the content is scrolled.
+
+		\a current is the index of the current displayed page,
+		\a total is the total number of pages.
+	*/
+	void contentScrolled(int current, int total);
 
 public slots:
+	/*!
+		\brief Switchs to the next page.
+
+		\note If you are on the last page, it switches to the first one.
+	*/
 	void nextItem();
+
+	/*!
+		\brief Switchs to the previous page.
+
+		\note If you are on the first page, it switches to the last one.
+	*/
 	void prevItem();
+
+	/*!
+		\brief Removes all the items.
+	*/
 	void clear();
 
 protected slots:
+	/*!
+		\brief Emits the itemIsClicked() signal.
+
+		\note \a item must be the index of the item into the current page.
+		For example, if ItemList::rows_per_page is 4, and you have 16 elements,
+		to make the itemIsClicked() signal to be emitted with 11 as argument,
+		you must be on the 3rd page and call this method with 4 as parameter.
+	*/
 	void clicked(int item);
 
 protected:
-	/// For pagination
+	/*!
+		\brief The maximum number of rows a page can have.
+	*/
 	int rows_per_page;
+
+	/*!
+		\brief Current page index
+	*/
 	int current_page;
 
 	QButtonGroup *buttons_group;
 	QVBoxLayout *main_layout;
 
-	/// The list of items displayed
+	/*!
+		\brief The list of items displayed.
+	*/
 	QList<ItemInfo> item_list;
 
+	/*!
+		\brief The calculated number of pages.
+	*/
 	int pageCount();
 
-	/**
-	 * addHorizontalBox
-	 *
-	 * In the default implementation, creates a box with a left icon, a label with
-	 * the \a item name, a label with the \a item description and a button.
-	 *
-	 * \note \a item.icons must contain at least 2 entries: one for the left icon and one for the button.
+	/*!
+		\brief Creates the widget used to represent an item.
+
+		In the default implementation, creates a box with a left icon, a label
+		with the \a item name, a label with the \a item description and a button.
+
+		\note \a item.icons must contain at least 2 entries: one for the left
+		icon and one for the button.
 	 */
 	virtual void addHorizontalBox(QBoxLayout *layout, const ItemInfo &item, int id_btn);
 };
